eda731400c4dc55dd834109a50cf0e07aadd29c0
Andrei Bissolotti <andreibisso@gmail.com>
2024-11-04 20:47:10 -0300

    Create a product use case

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -2,16 +2,20 @@ package controller
 
 import (
 	"go-api/model"
+	"go-api/usecase"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type productController struct {
+	productUseCase usecase.ProductUseCase
 }
 
-func NewProductController() productController {
-	return productController{}
+func NewProductController(usecase usecase.ProductUseCase) productController {
+	return productController{
+		productUseCase: usecase,
+	}
 }
 
 func (p *productController) GetProducts(ctx *gin.Context) {
---
+++ usecase/product_usecase.go
@@ -0,0 +1,13 @@
+package usecase
+
+import "go-api/model"
+
+type ProductUseCase struct{}
+
+func NewProductUseCase() ProductUseCase {
+	return ProductUseCase{}
+}
+
+func (pu *ProductUseCase) GetProducts() ([]model.Product, error) {
+	return []model.Product{}, nil
+}
