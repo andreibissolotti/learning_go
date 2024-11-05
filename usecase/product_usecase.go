69b658a53a409ef8b0d80dd9fc8f306a752152bf
Andrei Bissolotti <andreibisso@gmail.com>
2024-11-04 21:51:20 -0300

    Create logic to insert new products into database

--- usecase/product_usecase.go
+++ usecase/product_usecase.go
@@ -16,3 +16,14 @@ func NewProductUseCase(repository repository.ProductRepository) ProductUseCase {
 func (pu *ProductUseCase) GetProducts() ([]model.Product, error) {
 	return pu.repository.GetProducts()
 }
+
+func (pu *ProductUseCase) CreateProduct(product model.Product) (model.Product, error) {
+	productId, err := pu.repository.CreateProduct(product)
+	if err != nil {
+		return model.Product{}, err
+	}
+
+	product.ID = productId
+
+	return product, nil
+}
